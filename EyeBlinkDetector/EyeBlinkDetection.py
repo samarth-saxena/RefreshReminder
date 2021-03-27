7bdf1a2e72e7a15a3bbda8bf8f8630ce2136469b
ShreyMarwaha <55681068+ShreyMarwaha@users.noreply.github.com>
2021-03-27 21:31:16 +0530

    Add files via upload

---
+++ EyeBlinkDetector/EyeBlinkDetection.py
@@ -0,0 +1,48 @@
+from cv2 import cv2
+import numpy as np 
+import dlib
+
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+
+def midpoint(p1, p2):
+    return (p1.x+p2.x)//2, (p1.y+p2.y)//2;
+
+    
+while True:
+    _, frame = cap.read()
+    frame = cv2.flip(frame,1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = detector(gray)
+    for face in faces:
+        x, y = face.left(), face.top()
+        x1, y1 = face.right(), face.bottom()
+        cv2.rectangle(frame, (x,y), (x1,y1), (255,0,0), 3)
+        
+        #Left Eye Center // 36-->LeftEye leftSide 39-->LeftEye RightSide
+        landmarks = predictor(gray, face)
+        x = (landmarks.part(36).x + landmarks.part(39).x)//2
+        y = (landmarks.part(36).y + landmarks.part(39).y)//2
+        cv2.circle(frame, (x,y), 7, (0,0,255),2) #Red Cirle - eye center
+
+        left_point = (landmarks.part(36).x, landmarks.part(36).y)
+        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+        center_top = midpoint(landmarks.part(37), landmarks.part(38))
+        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+
+        hor_line = cv2.line(frame, left_point, right_point, (0,255,255),1)
+        ver_line = cv2.line(frame, center_top, center_bottom, (0,255,255),1)
+
+        print(landmarks)
+    
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    
+    if key==27: #Esc key to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
