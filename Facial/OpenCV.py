1383fda7b016b7efb8dc3ae63369afd449a3eadb
RitikVatsal-2019321 <58371368+RitikVatsal-2019321@users.noreply.github.com>
2021-03-30 21:39:00 +0530

    Update OpenCV.py

--- Facial/OpenCV.py
+++ Facial/OpenCV.py
@@ -14,16 +14,19 @@ while True:
     facialScan = getFace.detectMultiScale(gray, 1.1, 4)
 
     for (x, y, w, h) in facialScan:
-        if storeData == 0:
-            storeData = w * h
+        if (w*h > 3000):
+            if storeData == 0:
+                storeData = w * h
 
-        if w * h > 2 * storeData:
-            cv2.putText(frame, 'Too Close!', (x - 3, y - 3), font, 1, (255, 255, 255), 2)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        else:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # print(w*h)
 
-    cv2.imshow('bruh', frame)
+            if w * h > 2 * storeData:
+                cv2.putText(frame, 'Too Close!', (x - 3, y - 3), font, 1, (255, 255, 255), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            else:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('ComputerVision', frame)
     key = cv2.waitKey(1)
     if (key != -1):
         break
